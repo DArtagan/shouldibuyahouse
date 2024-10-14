e15df142ad15bbcb7df80798c6f81f13e17c6ec6
William Weiskopf <william@weiskopf.me>
2024-10-13 23:36:27 -0400

    Devenv & uv & jupyter working!

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    #nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    # Useful as a build cache, but let's not bother with it for now
+    #extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    #extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
+
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  languages.python = {
+                    enable = true;
+                    uv = {
+                      enable = true;
+                      sync.enable = true;
+                    };
+                  };
+                  packages = [ pkgs.gcc-unwrapped ];
+                  env.LD_LIBRARY_PATH = "${pkgs.gcc-unwrapped.lib}/lib64";
+                }
+              ];
+            };
+          });
+    };
+}
